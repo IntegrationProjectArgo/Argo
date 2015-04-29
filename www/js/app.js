0ee7e45b5880cb875bf1f26544ee7d37667b6297
GreyAragami <grey.descamps@gmail.com>
2015-04-29 16:19:20 +0200

    - putting map controller in quarantine

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'starter.directives', 'ionic.contrib.ui.cards', 'firebase'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'starter.directives' ,'starter.map', 'ionic.contrib.ui.cards', 'firebase'])
 
 .constant('ApiEndPoint', {
        url: 'http://vdabservices-cbt.vdab.be/vindeenjob/1.0.0/0/0'
